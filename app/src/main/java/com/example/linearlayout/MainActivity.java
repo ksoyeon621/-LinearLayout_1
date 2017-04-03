3f72ee6932c5f5d4f65a055537281b49e23ffd18
ksoyeon621 <ksoyeon621@naver.com>
2017-04-03 19:36:06 +0900

    #1 LinearLayout

---
+++ app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.linearlayout;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import static com.example.linearlayout.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
